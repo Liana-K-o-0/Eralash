37a3d5a32be731b7957d377a24c859f163a37626
Лиана Калажокова <lizakalazhokova@edu.hse.ru>
2026-05-29 22:46:44 +0300

    Update processor.py

--- processor.py
+++ processor.py
@@ -3,18 +3,20 @@ from pathlib import Path
 from dataclasses import dataclass
 import shutil
 from collections import defaultdict
-#import classifier
+from classifier import EmailClassifier
+from logger import Result_file
 
+result_file = Result_file()
+classifier = EmailClassifier()
 
 @dataclass
 class Email:
-    def __init__(self, file_path:Path, sender:str="", towho:str="", topic:str="", text:str="", file:str=""):
+    def __init__(self, file_path:Path, sender:str="", towho:str="", topic:str="", text:str=""):
         self.towho = towho
         self.sender = sender
         self.file_path = file_path
         self.text = text
         self.topic = topic
-        self.file = file
 
 def cycle_in_dir(dirpath : Path):
 
@@ -22,8 +24,9 @@ def cycle_in_dir(dirpath : Path):
         if item.is_file() and item.suffix.lower() == '.txt':
             create_letter_object(item)
         else:
-            #logger.error
-            ...
+            result_file.add_information('error',item.name)
+
+
 
 def create_letter_object(file_path):
     try:
@@ -49,20 +52,20 @@ def create_letter_object(file_path):
                         letter_object.text += stripped
                 else:
                     letter_object.text += stripped
-            #classifier.analyse
+            classifier.classify(letter_object)
     except Exception as e:
-        #logger.error
-        print(f"Ошибка: {e}")
+        result_file.add_information('error',file_path.name)
+        
 
 def put_to_folder(category:str,filepath):
 
-    base_dir = Path("")#вписать путь папки с папками
-    if not filepath.is_file():
-        print(f"Файл не найден: {filepath}")
-        #logger.error
-    target_dir = base_dir / category #папка с папками / category
+    outbox = Path("")#
+    # if not filepath.is_file():
+    #     print(f"Файл не найден: {filepath}")
+    target_dir = outbox / category 
     target_dir.mkdir(exist_ok=True)
     target_path = target_dir/filepath.name
     shutil.move(str(filepath),str(target_path))
 
-    #logger.info
+    result_file.add_information(category,filepath.name)
+    
