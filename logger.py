5f48f99fa593f396e295066d16a485a408a824f2
AyliNikes <ilyaprosexin2006@gmail.com>
2026-05-29 22:04:36 +0300

    Refactor result_file class and enhance functionality
    Refactored the result_file class to Result_file and added methods for logging and statistics.

--- logger.py
+++ logger.py
@@ -1,12 +1,31 @@
-class result_file:
+from collections import defaultdict
+import json
+class Result_file:
     def __init__(self):
-        self.result_file=
+        self.data=defaultdict(int)
+        self.log=[]
 
-
-    def add_information(self, file):
-        self.file=f
-
-class data_from_email:
-    def __init__(self, category):
-        self.category = category
+    def add_information(self, category, email_name):
+        self.data[category]+=1
+        if category != 'error':
+            self.log.append(f'{email_name} ---> {category}')
+        else:
+            self.log.append(f'{email_name} ---> Файл не удалось отнести ни к одной категории')
+        
+    def get_log_list(self):
+        return self.log
     
+    def get_stat_json(self):
+        return json.dumps(self.data, ensure_ascii=False)
+    
+result = Result_file()
+
+result.add_information('spam', 'email1')
+result.add_information('ham', 'email2')
+result.add_information('error', 'email3')
+
+log=result.get_log_list()
+for line in log:
+    print(line)
+stat_json=result.get_stat_json()
+print(stat_json)
