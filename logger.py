4b13d62b68d0c5f06bf8bdc3431d4f166af4071f
AyliNikes <ilyaprosexin2006@gmail.com>
2026-05-29 20:49:56 +0300

    Add result_file and data_from_email classes

---
+++ logger.py
@@ -0,0 +1,12 @@
+class result_file:
+    def __init__(self):
+        self.result_file=
+
+
+    def add_information(self, file):
+        self.file=f
+
+class data_from_email:
+    def __init__(self, category):
+        self.category = category
+    
