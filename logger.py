27d5f5303b46b37f01e6b01c17283ad5693713db
AyliNikes <ilyaprosexin2006@gmail.com>
2026-05-30 10:58:31 +0300

    Implement pie chart generation in logger
    Add a method to create a pie chart of logged data.

--- logger.py
+++ logger.py
@@ -1,5 +1,8 @@
 from collections import defaultdict
 import json
+import matplotlib.pyplot as plt
+#from processor import to_logger
+
 class Result_file:
     def __init__(self):
         self.data=defaultdict(int)
@@ -18,3 +21,18 @@ class Result_file:
     def get_stat_json(self):
         return json.dumps(self.data, ensure_ascii=False)
     
+    def make_pie(self):
+        if not self.data:
+            fig, ax = plt.subplots()
+            ax.text(0.5, 0.5, "Нет данных", ha="center", va="center")
+            ax.axis("off")
+            return fig
+
+        labels = [f"{cat}: {n}" for cat, n in self.data.items()]
+        values = list(self.data.values())
+
+        fig, ax = plt.subplots()
+        ax.pie(values, labels=labels, autopct="%1.0f%%", startangle=90)
+        ax.set_title("Письма по категориям")
+        ax.axis("equal")
+        return fig
